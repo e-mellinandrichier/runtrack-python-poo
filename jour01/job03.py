dd6ff3d2531b6a9f71def72cc43de9377954b8bd
Emmanuelle Mellinand-Richier <mellinand.emmanuelle@gmail.com>
2025-02-03 19:18:59 +0100

    job03

---
+++ jour01/job03.py
@@ -0,0 +1,11 @@
+class Operation: 
+    def __init__(self):
+        self.nombre1 = 1
+        self.nombre2 = 2
+
+    def addition(self):
+        result = self.nombre1 + self.nombre2
+        print(result)
+    
+
+Operation.addition(Operation())
