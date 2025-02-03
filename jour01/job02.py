dfb019a17124e317c3c9084157210d84ea09b386
Emmanuelle Mellinand-Richier <mellinand.emmanuelle@gmail.com>
2025-02-03 19:07:27 +0100

    job02

---
+++ jour01/job02.py
@@ -0,0 +1,8 @@
+class Operation: 
+    def __init__(self):
+        self.nombre1 = 1
+        self.nombre2 = 2
+
+x = Operation()
+print(f"Le nombre 1 est {x.nombre1}")
+print(f"Le nombre 2 est {x.nombre2}")
