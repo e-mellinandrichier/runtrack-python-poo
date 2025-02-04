505506bff33281c51fe0975710e94a2c2e3a4e31
Emmanuelle Mellinand-Richier <mellinand.emmanuelle@gmail.com>
2025-02-04 09:50:03 +0100

    created nommer method

--- jour01/job06.py
+++ jour01/job06.py
@@ -4,8 +4,15 @@ class Animal:
         self.prenom = ""
     
     def vieillir(self):
+        print(f"L'age de l'animal : {self.age} ans")
         self.age +=1
+        print(f"L'age de l'animal : {self.age} ans")
+
+    def nommer(self, nom):
+        self.prenom = nom
+        print(f"L'animal se nomme {self.prenom}")
+
 
 chien = Animal()
 chien.vieillir()
-print(chien.age)
+chien.nommer("Luna")
