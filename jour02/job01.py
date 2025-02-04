226ba249a01b98af5e8464c454f050185958f5ae
Emmanuelle Mellinand-Richier <mellinand.emmanuelle@gmail.com>
2025-02-04 19:26:27 +0100

    job01

---
+++ jour02/job01.py
@@ -0,0 +1,18 @@
+class Rectangle:
+    def __init__(self):
+        self.__longueur = 0
+        self.__largeur = 0
+
+    def getLongueur(self):
+        return self.__longueur
+
+    def getLargeur(self):
+        return self.__largeur
+    
+    def setLongueur(self, longueur):
+        self.__longueur = longueur
+    
+    def setLargeur(self,largeur):
+        self.__largeur = largeur
+
+
